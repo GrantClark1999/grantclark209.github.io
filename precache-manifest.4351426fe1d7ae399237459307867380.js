45f9e60c8cb6354877a6e31a9746821f5ac69ebe
GrantClark209 <grantclark1999@gmail.com>
2020-10-17 13:15:20 -0700

    Public build folder + change repo name

---
+++ precache-manifest.4351426fe1d7ae399237459307867380.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "882132f627c2914dbd1d2c6119e50dbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4070b57b902d6e4a55c",
+    "url": "/static/css/2.2c590ca5.chunk.css"
+  },
+  {
+    "revision": "0f4d661fe5b1c0d11218",
+    "url": "/static/css/main.c567a75b.chunk.css"
+  },
+  {
+    "revision": "f4070b57b902d6e4a55c",
+    "url": "/static/js/2.f704a4f1.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/static/js/2.f704a4f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f4d661fe5b1c0d11218",
+    "url": "/static/js/main.47f1a5e2.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  },
+  {
+    "revision": "38eeef6d24bd4e657e9e463cdc0abc37",
+    "url": "/static/media/AWS.38eeef6d.jpg"
+  },
+  {
+    "revision": "a7df7085e15f8060c39de266151dc471",
+    "url": "/static/media/C++.a7df7085.png"
+  },
+  {
+    "revision": "63dad4466af252691c4582fa14645964",
+    "url": "/static/media/ClarkFamily.63dad446.jpg"
+  },
+  {
+    "revision": "c6a9e60e25734119b83405e5266e93e7",
+    "url": "/static/media/Cruise.c6a9e60e.jpg"
+  },
+  {
+    "revision": "b0f998c15e76dc62e904b9a5c02786b2",
+    "url": "/static/media/Dogs.b0f998c1.jpg"
+  },
+  {
+    "revision": "924c9c79c551a472d4dab1f9be0a24ec",
+    "url": "/static/media/FuzBot.924c9c79.png"
+  },
+  {
+    "revision": "b86a482aa9c30a50bbdc739c868f8823",
+    "url": "/static/media/GC_Logo_Black.b86a482a.svg"
+  },
+  {
+    "revision": "57a584f8566dbcc39d3b0125c296fc70",
+    "url": "/static/media/Git.57a584f8.png"
+  },
+  {
+    "revision": "e6a4b8f01e125865e321c14e07e9eec3",
+    "url": "/static/media/Java.e6a4b8f0.png"
+  },
+  {
+    "revision": "6c8a9665ee8a3e251bad53748815cedd",
+    "url": "/static/media/NodeJS.6c8a9665.png"
+  },
+  {
+    "revision": "c71e1e048e0d45af6df547be66f58c2a",
+    "url": "/static/media/PKitty1.c71e1e04.jpg"
+  },
+  {
+    "revision": "a94084a73149dbaea42a606be35e443b",
+    "url": "/static/media/PKitty2.a94084a7.jpg"
+  },
+  {
+    "revision": "bd45cfa6e6704e1c3145b194ab3048f1",
+    "url": "/static/media/Python.bd45cfa6.png"
+  },
+  {
+    "revision": "0e6fc9259a52aee69ec2d73f07ee8403",
+    "url": "/static/media/RLMafia.0e6fc925.png"
+  },
+  {
+    "revision": "ed931db8893a909300aa1b8c2c81aab2",
+    "url": "/static/media/React.ed931db8.png"
+  },
+  {
+    "revision": "c3045a611c371b5454a60377179008fe",
+    "url": "/static/media/Redux.c3045a61.png"
+  },
+  {
+    "revision": "f9aba2b0576e26d6afc3061c57acc02f",
+    "url": "/static/media/SpringBoot.f9aba2b0.png"
+  },
+  {
+    "revision": "68db23797418adcd402a4cf3d5ab776b",
+    "url": "/static/media/Webpack.68db2379.png"
+  },
+  {
+    "revision": "bb0e03f7e45a3dfe1e78a84fff2943c2",
+    "url": "/static/media/Website.bb0e03f7.png"
+  },
+  {
+    "revision": "60b480ff55252a43e479167f8d24a010",
+    "url": "/static/media/background.60b480ff.jpg"
+  },
+  {
+    "revision": "2488ce57504fd75ecbb3eb08e30dd5b3",
+    "url": "/static/media/me.2488ce57.jpg"
+  }
+]);
